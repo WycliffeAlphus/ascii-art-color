f9b943f3a96319d50f2b550fd89440768df7b3f0
lakoth <lornaakothpb@gmail.com>
2024-04-30 17:53:39 +0300

    map function that maps ascii character to relevant patterned character from standard.txt

---
+++ mapPackage/map.go
@@ -0,0 +1,54 @@
+package mapPackage
+
+import (
+	"fmt"
+	"os"
+	"strings"
+)
+
+// func checkFile(){
+// 	args := os.Args
+// 	if len(args) != 2 {
+// 		fmt.Fprint(os.Stderr, `Usage<>
+// 		go run . (stringValue) -> go run . "Hello"
+// 		`)
+// 		os.Exit(1)
+// 	} else {
+// 		// input := args[1]
+// 		file := "standard.txt"
+// 		resultingMap := Map(file)
+// 		fmt.Println(resultingMap)
+// 	}
+// }
+
+var fileContentString []string
+
+func ReadStandard(fileName string) []string {
+	fileContents, err := os.ReadFile(fileName)
+	if err != nil {
+		fmt.Println("Error reading the file")
+		os.Exit(1)
+	} else {
+		fileContentString = strings.Split(string(fileContents), "\n")
+	}
+	return fileContentString
+}
+
+func Map(fileName string) map[rune][]string {
+	InputContents := ReadStandard(fileName)
+	ourMap := make(map[rune][]string)
+	start := ' '
+	i := 1
+	for i < len(InputContents) {
+		pattern := []string{}
+		for counter := 0; counter < 8; counter++ {
+			pattern = append(pattern, InputContents[i])
+			i++
+		}
+		i++
+		ourMap[start] = pattern
+		start++
+	}
+	fmt.Println(ourMap)
+	return ourMap
+}
