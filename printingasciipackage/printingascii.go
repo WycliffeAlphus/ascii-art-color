7f425f69972dc60ff812202f4e8cbedf30269ee9
wyonyango <wycliffealphus7@gmail.com>
2024-05-09 14:41:20 +0300

    Improved handling of \b,\t,and\r

--- printingasciipackage/printingascii.go
+++ printingasciipackage/printingascii.go
@@ -13,20 +13,26 @@ import (
 func PrintingAscii(text, patternFile string) string {
 	text = strings.ReplaceAll(text, "\n", "\\n")
 	res := ""
-	for i := range text {
+
+	for i := 0; i < len(text); {
 		if i+1 < len(text) && text[i] == '\\' && text[i+1] == 'a' {
 			fmt.Printf("error: Special character %v%v is not supported \n", string(text[i]), string(text[i+1]))
 			os.Exit(1)
 		}
 		if i+1 < len(text) && text[i] == '\\' && text[i+1] == 'b' {
-			text = text[:i-1] + text[i+2:]
+			if i == 0 {
+				text = text[i+2:]
+			} else {
+				text = text[:i-1] + text[i+2:]
+				i = 0
+			}
 			continue
 		}
 		if i+1 < len(text) && text[i] == '\\' && text[i+1] == 't' {
-			fmt.Printf("error: Special character %v%v is not supported \n", string(text[i]), string(text[i+1]))
-			os.Exit(1)
+			text = text[:i] + "   " + text[i+2:]
 		}
 		if i+1 < len(text) && text[i] == '\\' && text[i+1] == 'v' {
+
 			fmt.Printf("error: Special character %v%v is not supported \n", string(text[i]), string(text[i+1]))
 			os.Exit(1)
 		}
@@ -35,13 +41,20 @@ func PrintingAscii(text, patternFile string) string {
 			os.Exit(1)
 		}
 		if i+1 < len(text) && text[i] == '\\' && text[i+1] == 'r' {
-			fmt.Printf("error: Special character %v%v is not supported \n", string(text[i]), string(text[i+1]))
-			os.Exit(1)
+			if i == 0 {
+				text = text[i+2:]
+			} else {
+				text = text[i+2:]
+				i = 0
+			}
+
+			continue
 		}
 		if i+1 < len(text) && text[i] > 127 {
 			fmt.Println("error: Ascii Characters above 127 are not supported")
 			os.Exit(1)
 		}
+		i++
 	}
 	lines := strings.Split(text, "\\n")
 	asciiMap := mapPackage.AsciiMapping(patternFile)
