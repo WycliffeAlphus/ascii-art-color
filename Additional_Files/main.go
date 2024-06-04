4143d2bc27813e5c07ba6f10d4dc40616009997a
lakoth <lornaakothpb@gmail.com>
2024-06-04 12:16:07 +0300

    update: cleaned code

--- Additional_Files/main.go
+++ Additional_Files/main.go
@@ -53,15 +53,14 @@ func PrintingAscii(text, patternFile string) string {
 		}
 		i++
 	}
-	// text = FindSubString(text, "el")
-	alphaArray := findArr(text, "ow")
-	fmt.Println(alphaArray)
+	
 	lines := strings.Split(text, "\\n")
 	asciiMap := mapPackage.AsciiMapping(patternFile)
+	alphaArray := findSubStringIndex(text, "l")
+
 
 	count := 0
-	// alphaCount := 0 
-	for j, word := range lines { // case of multiple newlines
+	for wordIndex, word := range lines { // case of multiple newlines
 		if word == "" {
 			count++
 			if count < len(lines) {
@@ -69,25 +68,23 @@ func PrintingAscii(text, patternFile string) string {
 			}
 		} else {
 			for n := 0; n < 8; n++ {
-				for i, ch := range word {
+				for runeIndex, ch := range word {
 
-					if j > 0{
+					if wordIndex > 0{
 						checkIndex := 0
-						for x := 0; x < j; x++{
+						for x := 0; x < wordIndex; x++{
 							checkIndex += len(lines[x]) + 1
 						}
-						if intIsInArr(alphaArray, checkIndex+i+j){
+						if indicesInArr(alphaArray, checkIndex+runeIndex+wordIndex){
 							res += "\033[34m" + asciiMap[ch][n] + "\033[0m"
 							continue
 						}
 						res += asciiMap[ch][n]
 					} else {
-						if intIsInArr(alphaArray, i){
-							res += "\033[34m" + asciiMap[ch][n] + "\033[0m"
+						if indicesInArr(alphaArray, runeIndex){
+							res += "\033[33m" + asciiMap[ch][n] + "\033[0m"
 							continue
 						}
-	
-						// currentIndex = len(lines[j])
 						res += asciiMap[ch][n]
 					}
 
@@ -101,14 +98,14 @@ func PrintingAscii(text, patternFile string) string {
 }
 
 
-func isDuplicate(s1, s2 string) bool {
+func isSubstring(s1, s2 string) bool {
+
 	count := 0
 	for i:=0; i < len(s1);{
 		for j:=0; j < len(s2);{
 			if s1[i] == s2[j]{
 				count ++
-				j++
-				
+				j++	
 			}
 			i ++
 			if i == len(s1){
@@ -123,107 +120,20 @@ func isDuplicate(s1, s2 string) bool {
 }
 
 
-func findArr(mainString, subString string) []int {
-	res := []int{}
+func findSubStringIndex(mainString, subString string) []int {
+	indices := []int{}
 	for i:=0; i<len(mainString);i++{
-		fmt.Println("Here")
-		if  i+len(subString) <= len(mainString) && isDuplicate(subString, mainString[i:i+len(subString)]){
+		if  i+len(subString) <= len(mainString) && isSubstring(subString, mainString[i:i+len(subString)]){
 			for j:=i ; j < i+len(subString);j++{
 				
-				res = append(res, j)
+				indices= append(indices, j)
 			}
 		}
 	}
-	fmt.Println(res)
-	return res
+	return indices
 }
 
-
-
-
-func FindSubString(mainString, subString string) [][]int {
-	s1 := []string{}
-
-	resString := ""
-
-	intArr := []int{}
-
-	intArrArr := [][]int{}
-
-	// Convert mainString to a slice of strings
-	for i := 0; i < len(mainString); i++ {
-		s1 = append(s1, string(mainString[i]))
-	}
-
-	// Iterate through the main string
-	for i := 0; i < len(s1); {
-		// Iterate through the substring
-		for j := 0; j < len(subString); {
-			// Check if characters match
-			if string(subString[j]) == s1[i] {
-				intArr = append(intArr, i)
-
-				currIndex := intArr[len(intArr)-1]
-				prevIndex := 0
-				if len(intArr) < 2 {
-					prevIndex = i - 1
-				} else {
-					prevIndex = intArr[len(intArr)-2]
-				}
-
-				indexDiff := currIndex - prevIndex
-
-				j++
-
-				// Reset if characters are not adjacent
-				if indexDiff > 1 && len(subString) > 1 {
-					j = 0
-					intArr = []int{}
-				}
-
-			}
-
-			// Store the index array if the substring is found
-			if j == len(subString) {
-				intArrArr = append(intArrArr, intArr)
-			}
-
-			i++
-
-			// Break if end of main string is reached
-			if i == len(s1) {
-				break
-			}
-		}
-	}
-
-	// Return message if substring not found
-	if len(intArrArr) < 1 {
-		msg := "$" + subString + "$ is not present in $" + mainString + "$"
-		fmt.Println(msg)
-		os.Exit(0)
-		// return msg
-	}
-
-	// Highlight the substring in the main string
-	for _, ch := range intArrArr {
-		for i := 0; i < len(ch); i++ {
-			s1[ch[i]] = "\033[34m" + s1[ch[i]] + "\033[0m"
-		}
-	}
-
-	// Construct the resulting string
-	for i := 0; i < len(s1); i++ {
-		resString += s1[i]
-	}
-
-	// return resString
-	return intArrArr
-}
-
-
-
-func intIsInArr(arr []int, num int) bool {
+func indicesInArr(arr []int, num int) bool {
 	for _,x := range arr{
 		
 			if x == num{
@@ -236,5 +146,5 @@ func intIsInArr(arr []int, num int) bool {
 
 
 func main() {
-	fmt.Print(PrintingAscii("Hellow \nworld \n owmy \nmyow", "standard.txt"))
+	fmt.Print(PrintingAscii("yellow\nblue", "standard.txt"))
 }
