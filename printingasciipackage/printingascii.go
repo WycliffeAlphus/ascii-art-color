2e7d3b1647057d7e9fff62bfd4ce394bb2c73788
gonyango <grantononyango@gmail.com>
2024-06-05 18:33:11 +0300

    Feat: decluter PrintingAscii Function

--- printingasciipackage/printingascii.go
+++ printingasciipackage/printingascii.go
@@ -1,12 +1,17 @@
 package printingasciipackage
+
 import (
 	"fmt"
 	"os"
 	"strings"
+
 	"ascii-art-color/mapPackage"
 	"ascii-art-color/utils"
 )
+
 // Reads input text,gets the pattern convert it to ascii art
+func PrintingAscii(text, patternFile, color, subString string) string {
+	// fmt.Println(len(color))
 	text = strings.ReplaceAll(text, "\n", "\\n")
 	res := ""
 	for i := 0; i < len(text); {
@@ -30,7 +35,6 @@ import (
 			text = text[:i] + "   " + text[i+2:]
 		}
 		if i+1 < len(text) && text[i] == '\\' && text[i+1] == 'v' {
-
 			fmt.Fprintf(os.Stderr, "error: Special character %v%v is not supported \n", string(text[i]), string(text[i+1]))
 			os.Exit(1)
 		}
@@ -50,11 +54,9 @@ import (
 	}
 	lines := strings.Split(text, "\\n")
 	asciiMap := mapPackage.AsciiMapping(patternFile)
-
-	
-	if !strings.Contains(text,letters){
-		count := 0
-	for _, word := range lines { // case of multiple newlines
+	alphaArray := utils.FindSubStringIndex(text, subString)
+	count := 0
+	for wordIndex, word := range lines { // case of multiple newlines
 		if word == "" {
 			count++
 			if count < len(lines) {
@@ -62,71 +64,30 @@ import (
 			}
 		} else {
 			for n := 0; n < 8; n++ {
-				for _, ch := range word {
-					if len(letters) == 0{
-						res += color + asciiMap[ch][n] + "\033[0m"
-					} else {
-						if isInside(string(ch), letters){
-							res +=  color + asciiMap[ch][n] + "\033[0m" 
+				// fmt.Println(color)
+				for runeIndex, ch := range word {
+					if wordIndex > 0 {
+						checkIndex := 0
+						for x := 0; x < wordIndex; x++ {
+							checkIndex += len(lines[x]) + 1
+						}
+						if utils.IndicesInArr(alphaArray, checkIndex+runeIndex+wordIndex) {
+
+							res += color + asciiMap[ch][n] + "\033[0m"
 							continue
 						}
 						res += asciiMap[ch][n]
+					} else {
+						if utils.IndicesInArr(alphaArray, runeIndex) {
+							res += color + asciiMap[ch][n] + "\033[0m"
+							continue
 						}
+						res += asciiMap[ch][n]
+					}
 				}
 				res += "\n"
 			}
 		}
-		return res
-	} 
-	}else{
-			alphaArray:=colorfeature.FindSubStringIndex(text,letters)
-			count:=0
-			for wordIndex, word := range lines { // case of multiple newlines
-				if word == "" {
-					count++
-					if count < len(lines) {
-						res += "\n"
-					}
-				} else {
-					for n := 0; n < 8; n++ {
-						for runeIndex, ch := range word {
-		                    if wordIndex > 0{
-								checkIndex := 0
-								for x := 0; x < wordIndex; x++{
-									checkIndex += len(lines[x]) + 1
-								}
-								if  colorfeature.IndicesInArr(alphaArray, checkIndex+runeIndex+wordIndex){
-									res += color + asciiMap[ch][n] + "\033[0m"
-									continue
-								}
-								res += asciiMap[ch][n]
-							} else {
-								if  colorfeature.IndicesInArr(alphaArray, runeIndex){
-									res += color + asciiMap[ch][n] + "\033[0m"
-									continue
-								}
-								res += asciiMap[ch][n]
-							}
-		
-							
-						}
-						res += "\n"
-					}
-				}
-			}
-			
-		}
-		return res
 	}
-		
-	func isInside(alpha, s1 string) bool {
-		for _, ch := range s1{
-			if alpha == string(ch){
-				return true
-			}
-		}
-		return false
-	}
-
-
-	
+	return res
+}
