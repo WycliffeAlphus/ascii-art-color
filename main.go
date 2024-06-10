5dc8fe7dff71d46d4dfd7e579e7aa7890d8a3cd7
wyonyango <wycliffealphus7@gmail.com>
2024-06-10 14:00:51 +0300

    style: improve error message

--- main.go
+++ main.go
@@ -36,10 +36,6 @@ EX: go run . --color=<color> <letters to be colored> "something"`)
 		ap := printingasciipackage.PrintingAscii(os.Args[3], "standard.txt", color, os.Args[2])
 		fmt.Print(ap)
 	}
-
-	// fmt.Println(RGBtoAnsi(),0,0))
-
-	// fmt.Println(stringSplitter("rgb(255, 0, 0)"))
 }
 
 func color(color string) string {
@@ -64,7 +60,7 @@ func ansiColor(asciiNum int) string {
 
 func FindingColor(s string) string {
 	colorWanted := ""
-	for i, ch := range s { // finds color user wants
+	for i, ch := range s {
 		if ch == '=' {
 			colorWanted = s[i+1:]
 			break
@@ -85,25 +81,22 @@ func FindingColor(s string) string {
 	} else {
 		color := color(colorWanted)
 		if color == "" {
-			fmt.Println("Kindly use the RGB value of color", colorWanted)
+			fmt.Println(`Usage: go run . [OPTION] [STRING]
+
+EX: go run . --color=<color> <substring to be colored> "something"
+				`)
 			os.Exit(0)
 		}
 		return color
 	}
-
-	// color := color(colorWanted)
-	// return "color"
 }
 
 func RGBtoAnsi(R, G, B int) int {
-	// 16-231:  6 × 6 × 6 cube (216 colors): 16 + 36 × r + 6 × g + b (0 ≤ r, g, b ≤ 5)
 	r := R / 51
 	g := G / 51
 	b := B / 51
 
 	colorNum := 16 + 36*r + 6*g + b
-
-	fmt.Println(colorNum)
 
 	return colorNum
 }
@@ -111,19 +104,19 @@ func RGBtoAnsi(R, G, B int) int {
 func stringSplitter(s string) []int {
 	res := []int{}
 
-	// --color=rgb(255, 0, 0)
-
 	editedString := strings.TrimLeft(s, "rgb(")
 	editedString = strings.TrimRight(editedString, ")")
 
 	stringSplit := strings.Split(editedString, ",")
 
 	for _, ch := range stringSplit {
-		number, _ := strconv.Atoi(strings.TrimSpace(ch))
+		number, err := strconv.Atoi(strings.TrimSpace(ch))
 		res = append(res, number)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Error converting string %v to integer\n", ch)
+			os.Exit(0)
+		}
 	}
-
-	// fmt.Println(res)
 
 	return res
 }
