aa0784c45c076f41c3f697cba5c9a5b30c857154
lakoth <lornaakothpb@gmail.com>
2024-06-11 16:38:56 +0300

    Feat:Add cover another representation of hex code

--- utils/hexToDec.go
+++ utils/hexToDec.go
@@ -12,31 +12,43 @@ func HexToDec(s string) []int {
 			fmt.Fprintf(os.Stderr, "%v is not a valid hex color code\n", s)
 			os.Exit(0)
 		}
-	
 	}
 	newString := s[1:]
-	if len(newString) != 6 {
-		
-		fmt.Fprintf(os.Stderr, "%v is not a valid hex color> code\n", s)
-		os.Exit(0)
-	}
 	var hexArray []string
 	var word string
 	var count int
-	for _, char := range newString {
-		count++
-		word += string(char)
-		if count%2 == 0 {
-			hexArray = append(hexArray, word)
-			word = ""
+	if len(newString) != 6 && len(newString) != 3 {
+
+		fmt.Fprintf(os.Stderr, "%v is not a valid hex color> code\n", s)
+		os.Exit(0)
+	}
+	if len(newString) == 3 {
+		for _, ch := range newString {
+			word += string(ch) + string(ch)
+			count++
+			if count == 1 {
+				hexArray = append(hexArray, word)
+				word = ""
+			}
+			count = 0
+		}
+	} else {
+		for _, char := range newString {
+			count++
+			word += string(char)
+			if count%2 == 0 {
+				hexArray = append(hexArray, word)
+				word = ""
+
+			}
 
 		}
-
 	}
+
 	var hexIntSlice []int
 	for _, ch := range hexArray {
 		dec, err := strconv.ParseInt(ch, 16, 64)
-		if err != nil{
+		if err != nil {
 			fmt.Fprintf(os.Stderr, "%v is not a valid hex color code\n", s)
 			os.Exit(0)
 		}
