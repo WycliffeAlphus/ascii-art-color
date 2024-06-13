49f4a847e40d594c29d137a6222e4cc4bbe956f6
gonyango <grantononyango@gmail.com>
2024-06-13 17:35:46 +0300

    Docs: added function description to IndicesInArr function

--- utils/indicesInArr.go
+++ utils/indicesInArr.go
@@ -1,5 +1,6 @@
 package utils
 
+/*The IndicesInArr function checks if a given integer num exists in a slice of integers arr.*/
 func IndicesInArr(arr []int, num int) bool {
 	for _, x := range arr {
 		if x == num {
