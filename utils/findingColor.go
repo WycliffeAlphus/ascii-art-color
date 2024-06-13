4b5cd839d44515c25d3542dfab89bb1ed48dd76d
wyonyango <wycliffealphus7@gmail.com>
2024-06-13 10:46:05 +0300

    style: format function

--- utils/findingColor.go
+++ utils/findingColor.go
@@ -26,13 +26,13 @@ func FindingColor(s string) string {
 		ansiNum := RGBtoAnsi(rgbarray[0], rgbarray[1], rgbarray[2])
 		color := ansiColor(ansiNum)
 		return color
-	} else if strings.Contains(colorWanted,"#"){
-		hexInt:=HexToDec(colorWanted)
-		ansiNum:=RGBtoAnsi(hexInt[0],hexInt[1],hexInt[2])
-        color:=ansiColor(ansiNum)
+	} else if strings.Contains(colorWanted, "#") {
+		hexInt := HexToDec(colorWanted)
+		ansiNum := RGBtoAnsi(hexInt[0], hexInt[1], hexInt[2])
+		color := ansiColor(ansiNum)
 		return color
 
-	}else {
+	} else {
 		color := color(colorWanted)
 		if color == "" {
 			fmt.Println(`Usage: go run . [OPTION] [STRING]
