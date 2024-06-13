f77f806eae29e29e3803a86854d956dbeaab87ae
gonyango <grantononyango@gmail.com>
2024-06-13 17:34:42 +0300

    Docs: added function description to FindSubStringIndex function

--- utils/findSubStringIndex.go
+++ utils/findSubStringIndex.go
@@ -1,15 +1,16 @@
 package utils
 
+/*The FindSubStringIndex function finds all occurrences 
+of a substring within a given string and returns a slice of the 
+indices where each character of the substring occurs in the main string. */
 func FindSubStringIndex(mainString, subString string) []int {
 	indices := []int{}
 	for i := 0; i <= len(mainString)-len(subString); i++ {
-		// fmt.Println(mainString[i : i+len(subString)])
 		if mainString[i:i+len(subString)] == subString {
 			for j := i; j < i+len(subString); j++ {
 				indices = append(indices, j)
 			}
 		}
 	}
-	// fmt.Println(indices)
 	return indices
 }
